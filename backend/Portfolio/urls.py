7357146b572b56152a013207f237d8d039a7e03d
alvaro.gil <alvaro.gil@mercanza.es>
2024-09-02 16:02:56 +0100

    fdaaaaaaa

--- backend/Portfolio/urls.py
+++ backend/Portfolio/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from principal.views import MyModelViewSet
-
+#warever
 
 router = DefaultRouter()
 router.register(r'mymodel', MyModelViewSet)
