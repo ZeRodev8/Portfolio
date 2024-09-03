7d2cf2293b811022b41a300d44ff948891ebc82f
alvaro.gil <alvaro.gil@mercanza.es>
2024-09-03 09:53:29 +0100

    Prepared

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -15,16 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from principal.views import MyModelViewSet
-#warever
-
-router = DefaultRouter()
-router.register(r'mymodel', MyModelViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+
 
 ]
